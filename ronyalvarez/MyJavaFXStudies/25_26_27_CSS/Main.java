8864292577c7d4fe2737b0676165489f3032707e
Rony Alvarez <ronyalvarez1993@gmail.com>
2016-07-18 18:21:16 -0400

    Added tutorials 24 to 27

---
+++ ronyalvarez/MyJavaFXStudies/25_26_27_CSS/Main.java
@@ -0,0 +1,69 @@
+import com.intellij.codeInsight.intention.impl.ConcatenationToMessageFormatAction;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("JavaFX - Rony Alvarez");
+
+        //GridPane with 10px padding around edge
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
+
+        //Name Label - constrains use (child, column, row)
+        Label nameLabel = new Label("Username:");
+        nameLabel.setId("bold-label");
+        GridPane.setConstraints(nameLabel, 0, 0);
+
+        //Name Input
+        TextField nameInput = new TextField("Bucky");
+        GridPane.setConstraints(nameInput, 1, 0);
+
+        //Password Label
+        Label passLabel = new Label("Password:");
+        GridPane.setConstraints(passLabel, 0, 1);
+
+        //Password Input
+        TextField passInput = new TextField();
+        passInput.setPromptText("password");
+        GridPane.setConstraints(passInput, 1, 1);
+
+        //Login
+        Button loginButton = new Button("Log In");
+        GridPane.setConstraints(loginButton, 1, 2);
+
+        //Sign up
+        Button signUpButton = new Button("Sign Up");
+        signUpButton.getStyleClass().add("button-blue");
+        GridPane.setConstraints(signUpButton, 1, 3);
+
+        //Add everything to grid
+        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton, signUpButton);
+
+        Scene scene = new Scene(grid, 300, 200);
+        scene.getStylesheets().add("Viper.css");
+        window.setScene(scene);
+        window.show();
+    }
+
+}
